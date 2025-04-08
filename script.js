7fb041762b08422fa8da8984c412e5d20c3fa08c
nonhr <ashtonsubi975@gmail.com>
2025-04-08 19:42:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Cloak button functionality to change the tab title
+document.querySelector(".cloaker").addEventListener("click", function() {
+    let newTitle = prompt("Enter a new tab name:");
+    if (newTitle) {
+        document.title = newTitle;
+    }
+});
 
